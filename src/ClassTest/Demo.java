af288cfe075383e47736a5b5253dfbc62012c023
李聪盛 <515478826@qq.com>
2018-12-15 00:16:57 +0800

    关于类的测试

---
+++ src/ClassTest/Demo.java
@@ -0,0 +1,10 @@
+package ClassTest;
+
+public class Demo {
+    public static void main(String[] args)
+    {
+        People test_people = new People("MeowMeow",Sex.male);
+        test_people.set_age(14);
+        test_people.show_info();
+    }
+}
---
+++ src/ClassTest/People.java
@@ -0,0 +1,32 @@
+package ClassTest;
+
+enum Sex {
+    female,
+    male,
+    unknown,
+}
+
+// 测试一下类的写法
+public class People {
+    private String name;
+    private int age = 0;
+    private Sex sex;
+
+    People(String name, Sex sex) {
+        this.name = name;
+        this.sex = sex;
+    }
+
+    void set_age(int age) {
+        this.age = age;
+    }
+
+    public int get_age() {
+        return this.age;
+    }
+    void show_info(){
+        System.out.println("name = " + this.name);
+        System.out.println("age = " + this.age);
+        System.out.println("sex = " + this.sex);
+    }
+}
