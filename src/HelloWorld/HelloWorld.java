393f4b8ef6a0d5e15ea8aa0c5a82bedface1e149
李聪盛 <515478826@qq.com>
2018-12-15 00:40:12 +0800

    修改了一下代码分格 好爽

--- src/HelloWorld/HelloWorld.java
+++ src/HelloWorld/HelloWorld.java
@@ -1,9 +1,12 @@
 package HelloWorld;
+
 // 我的第一个java程序
 // hello world
 // 我想 这也是许多人的第一个java程序
-public class HelloWorld {
-    public static void main(String[] args) {
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
         System.out.println("HelloWorld");
     }
 }
