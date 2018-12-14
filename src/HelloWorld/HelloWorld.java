0b076baa61f857625ec4cbfe3871c9c8a320839b
李聪盛 <515478826@qq.com>
2018-12-14 23:51:03 +0800

    第一个java程序

--- src/HelloWorld/HelloWorld.java
+++ src/HelloWorld/HelloWorld.java
@@ -1,5 +1,7 @@
 package HelloWorld;
-
+// 我的第一个java程序
+// hello world
+// 我想 这也是许多人的第一个java程序
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("HelloWorld");
