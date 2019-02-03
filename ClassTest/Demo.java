f0e27b6507ab7196136f8d9c4506d9f8453c22ff
李聪盛 <515478826@qq.com>
2019-02-04 02:06:31 +0800

    ojbk

--- ClassTest/Demo.java
+++ ClassTest/Demo.java
@@ -1,10 +1,8 @@
 package ClassTest;
 
 // 程序的入口
-public class Demo
-{
-    public static void main(String[] args)
-    {
+public class Demo {
+    public static void main(String[] args) {
         People test_people = new People("MeowMeow", Sex.male);
         test_people.set_age(14);
         test_people.show_info();
