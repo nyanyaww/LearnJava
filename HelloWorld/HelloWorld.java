c7f81c9e1e1858a121b920c63e3b7b30bf82e049
nyanyaww <515478826@qq.com>
2019-02-03 23:09:53 +0800

    我命都没有了

--- HelloWorld/HelloWorld.java
+++ HelloWorld/HelloWorld.java
@@ -1,13 +1,10 @@
 package HelloWorld;
 
-
 // 我的第一个java程序
 // hello world
 // 我想 这也是许多人的第一个java程序
-public class HelloWorld
-{
-    public static void main(String[] args)
-    {
+public class HelloWorld {
+    public static void main(String[] args) {
         System.out.println("HelloWorld");
     }
 }
