d0e8d39db5282ac7234cafe36d85ed8a65ccdfbb
李聪盛 <515478826@qq.com>
2019-02-06 21:50:29 +0800

    总算改好了如何如用makeflie

--- ClassTest/Demo.java
+++ ClassTest/Demo.java
@@ -1,10 +1,14 @@
 package ClassTest;
 
+
 // 程序的入口
 public class Demo {
     public static void main(String[] args) {
-        People test_people = new People("MeowMeow", Sex.male);
-        test_people.set_age(14);
-        test_people.show_info();
+        People testPeople = new People("name", Sex.unknown);
+        // People test_people = new ClassTest.People("MeowMeow", Sex.male);
+        // test_people.set_age(14);
+        // test_people.show_info();
+        testPeople.set_age(21);
+        testPeople.show_info();
     }
 }
--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 记录我学习java的过程
 
-1.HelloWorld里面是我第一个java程序
+1. HelloWorld里面是我第一个java程序
 
-2.ClassTest是我测试类的写法用的
+2. ClassTest是我测试类的写法用的
