805ebc0307d807538ff96f670c8c21540c029f18
李聪盛 <515478826@qq.com>
2019-02-06 22:31:03 +0800

    111

---
+++ DataTest/DataTest.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+
+public class DataTest {
+    public static void main(String[] args) {
+        System.out.println("基本类型：byte 二进制位数：" + Byte.TYPE);  
+        System.out.println(new Date());
+    }
+}
